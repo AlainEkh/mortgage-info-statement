050fc1f5cd4a1a3058c8b0bd33aa0d0514924dda
AlainEkh <alain.ekmekdjian@outlook.com>
2025-06-13 18:00:30 -0400

    cleaned up files and created starter template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div style={{ padding : '20px'}} >
+            <h1>Mortgage Info Statement Generator</h1>
+            <p>Upload your HTML file below:</p>
+        </div>
+    )
 }
 
 export default App;
